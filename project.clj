8d91ca9ddc13a45fabcc531ed25002da00a04340
Anton Maklakov <anton.maklakov@vincit.com>
2017-06-02 10:13:47 +0300

    Bump version to 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vincit/venia "0.1.3-SNAPSHOT"
+(defproject vincit/venia "0.2.1-SNAPSHOT"
   :description "Clojure(Script) graphql client"
   :url "https://github.com/Vincit/venia"
   :license {:name "Eclipse Public License"
