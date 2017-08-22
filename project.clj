d1c41f963ce2200a10a430a351d2fe2c30c04c21
Anton Maklakov <anton.maklakov@vincit.com>
2017-08-22 08:36:21 +0300

    Update cljs version to 1.9.908

--- project.clj
+++ project.clj
@@ -4,13 +4,14 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.562" :scope "provided"]]
+                 [org.clojure/clojurescript "1.9.908" :scope "provided"]]
   :plugins [[lein-doo "0.1.7"]]
   :clean-targets ^{:protect false} ["resources" "target"]
   :aliases {"test" ["do" "test" ["doo" "once" "phantom" "test"]]}
   :cljsbuild {:builds [{:id           "test"
                         :source-paths ["src" "test"]
                         :compiler     {:output-to     "resources/test/js/unit-test.js"
+                                       :process-shim  false
                                        :main          venia.runner
                                        :optimizations :none
                                        :pretty-print  true
